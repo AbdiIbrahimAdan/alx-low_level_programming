6d009e0f3b62eeddeef4159ed539cbd6328beb9b
Abdi <abdishu98@gmail.com>
2023-06-19 01:39:41 -0700

    this file 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 
 /**
- * main - print alphabet 
+ *main -print alphabet. 
  *
  * Return:Always 0 (success)
  */
 
 int main(void)
-{
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+{  
+        char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
